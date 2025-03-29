5ee4948c5955ec1168a1d3d3dd8fe4217ee76e94
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2025-03-29 11:47:49 +0900

    書籍の追加と検索機能を実装するために、BookControllerを追加し、エンドポイントを設定

--- library-app/src/app.ts
+++ library-app/src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import { BookController } from './presentation/bookController';
 
 const app = express();
 
 app.use(express.json());
 
+const bookController = new BookController();
+
 const PORT = process.env.PORT || 3030;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World' });
-});
+app.post('/books', bookController.add.bind(bookController)); // bind を利用することで this が確実に bookController を参照するようにしている
+app.get('/books/:id', bookController.findById.bind(bookController));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
